36552a7f0e8545271ba3ea2c22e8c13bb06301f6
Krishnakant Bhargava <krribss@Krishnakants-MacBook-Air.local>
2022-06-08 00:15:42 +0530

    app launch check

---
+++ DjangoDemo/first_app/urls.py
@@ -0,0 +1,13 @@
+#from django.conf.urls import url
+from django.urls import path
+
+from first_app import views
+
+
+
+urlpatterns=[
+        path('',views.index,name='index'),
+
+
+]
+print(3)
---
+++ ProTwo/AppTwo/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from AppTwo import views
+
+urlpatterns = [
+
+    path('',views.help, name='help'),
+
+]
